7a995edd0c5e6c568181462db647aeefb59d9b31
marceelaaxh <a01658023@tec.mx>
2025-05-02 20:59:18 -0600

    fix: fixed several things

--- src/main/java/com/alcozone/infrastructure/rest/LoginController.java
+++ src/main/java/com/alcozone/infrastructure/rest/LoginController.java
@@ -1,9 +1,6 @@
 package com.alcozone.infrastructure.rest;
 
-import com.alcozone.application.service.UserService;
-import com.alcozone.infrastructure.dto.user.UserDTO;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
@@ -15,14 +12,10 @@ import jakarta.ws.rs.container.ContainerRequestContext;
 @Produces(MediaType.APPLICATION_JSON)
 public class LoginController {
 
-    @Inject
-    UserService userService;
 
     @GET
     @Path("/login")
-    public Response login(@Context ContainerRequestContext ctx) {
-        String uuid = (String) ctx.getProperty("userUuid");
-        UserDTO user = userService.findUserByFirebaseUid(uuid);
-        return Response.ok(user).build();
+    public Response me(@Context ContainerRequestContext ctx) {
+        return Response.ok().build();
     }
 }
