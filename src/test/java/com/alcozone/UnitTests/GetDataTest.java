d8bf23924a9bdc68e5ae43d648e18ffbd86d38f9
Mariana Fernandez <A01657317@tec.mx>
2025-06-04 19:59:26 -0600

    pruebas completas yupi

---
+++ src/test/java/com/alcozone/UnitTests/GetDataTest.java
@@ -0,0 +1,67 @@
+package com.alcozone.UnitTests;
+
+import com.alcozone.application.service.WidgetService;
+import com.alcozone.infrastructure.dto.widget.AccidentNumberDTO;
+import com.alcozone.infrastructure.persistence.crash.CrashEntity;
+import com.alcozone.infrastructure.persistence.revision.RevisionEntity;
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+@QuarkusTest
+public class GetDataTest {
+
+    @Inject
+    WidgetService widgetService;
+
+    @Test
+    @Transactional
+    public void testInsertData() {
+        RevisionEntity revision = new RevisionEntity();
+        revision.persist();
+
+        for (int i = 0; i < 25279; i++) {
+            CrashEntity crash = new CrashEntity();
+            crash.setUuid("uuid_" + i);
+            crash.setDatetime("2025-06-01T12:00:00");
+            crash.setType("Colisión");
+            crash.setSubType("Choque con lesionados");
+            crash.setReportedBy("Test User");
+            crash.setTown("CDMX");
+            crash.setNeighbourhood("Centro");
+            crash.setLatitude(19.4326);
+            crash.setLongitude(-99.1332);
+            crash.setRevisionEntity(revision);
+            crash.persist();
+        }
+
+        List<AccidentNumberDTO> accidentStats = widgetService.getAccidentsNumber();
+        AccidentNumberDTO firstResult = accidentStats.getFirst();
+
+        System.out.println("SubType: " + firstResult.getSubType() + " - Count: " + firstResult.getAccidentCount());
+
+        assert firstResult.getSubType().equals("Choque con lesionados");
+        assert firstResult.getAccidentCount() == 25279;
+    }
+
+    @Test
+    @Transactional
+    public void testInsertCrash_Failed_EmptyList() {
+        List<AccidentNumberDTO> accidentStats = widgetService.getAccidentsNumber();
+
+        if (accidentStats == null || accidentStats.isEmpty()) {
+            System.out.println("No se pudieron ingresar datos");
+            assert true;
+        } else {
+
+            AccidentNumberDTO firstResult = accidentStats.getFirst();
+            System.out.println("Se encontraron datos inesperados: SubType: " + firstResult.getSubType() + " - Count: " + firstResult.getAccidentCount());
+            assert false : "Se esperaban cero datos, pero se encontraron datos";
+        }
+    }
+
+}
+
