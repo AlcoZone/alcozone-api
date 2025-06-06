745e1d0669061acbff2d8017b07c143f1b0b8d93
Alondra Garcia <a01663185>
2025-06-06 15:36:31 -0600

    Filtro de fechas

--- src/main/java/com/alcozone/infrastructure/mapper/widgetdata/WidgetFiltersMapper.java
+++ src/main/java/com/alcozone/infrastructure/mapper/widgetdata/WidgetFiltersMapper.java
@@ -7,12 +7,16 @@ public class WidgetFiltersMapper {
     public static WidgetFilters toDomain(WidgetFiltersDTO dto){
         WidgetFilters filters = new WidgetFilters();
         filters.setTown(dto.getTown());
+        filters.setStartDate(dto.getStartDate());
+        filters.setEndDate(dto.getEndDate());
         return filters;
     }
 
     public static WidgetFiltersDTO toDTO(WidgetFilters filters){
         WidgetFiltersDTO dto = new WidgetFiltersDTO();
         dto.setTown(filters.getTown());
+        dto.setStartDate(filters.getStartDate());
+        dto.setEndDate(filters.getEndDate());
         return dto;
     }
 }
